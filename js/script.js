84454971328872dc1a7d9901d5cc3193f51efc2a
Andrea <andreademarco91@gmail.com>
2020-01-23 17:11:44 +0100

    JS FIRST

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-$(document).ready() {
-  $('.send_button').click(function() {
-    var messaggio = $('.message').value();
-    console.log(messaggio);
-
-    }
-  )
-}
+$(document).ready (
+  function() {
+    $('.send_button').click(
+      function() {
+        var testoDaInviare = $('.message').val();
+        console.log(testoDaInviare);
+        var nuovoMessaggio = $('.message_out_hide').clone();
+        nuovoMessaggio.children('p').text(testoDaInviare)
+        console.log(nuovoMessaggio);
+        $('.chat__main').append(nuovoMessaggio)
+      }
+    );
+  }
+);
