674a2a9d189eeb93a8930cc004378a19aac39814
Andrea <andreademarco91@gmail.com>
2020-01-23 17:41:26 +0100

    JS WORKS!!!!!!!!!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready (
       function() {
         var testoDaInviare = $('.message').val();
         console.log(testoDaInviare);
-        var nuovoMessaggio = $('.message_out_hide').clone();
+        var nuovoMessaggio = $('.template div').clone();
         nuovoMessaggio.children('p').text(testoDaInviare)
         console.log(nuovoMessaggio);
         $('.chat__main').append(nuovoMessaggio)
