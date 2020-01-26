4cc752c2eff1afb32f3e47c7b4b071c4b2312677
Andrea <andreademarco91@gmail.com>
2020-01-26 18:53:29 +0100

    MILESTONE 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,61 @@
-$(document).ready (
-  function() {
-    $('.send_button').click(
+$(document).ready(function() {
+  $(document).on("click", ".send_button",
       function() {
-        var testoDaInviare = $('.message').val();
-        console.log(testoDaInviare);
-        var nuovoMessaggio = $('.template div').clone();
-        nuovoMessaggio.children('p').text(testoDaInviare)
-        console.log(nuovoMessaggio);
-        $('.chat__main').append(nuovoMessaggio)
-        nuovoMessaggio.show()
+        sendMessage();
+        setTimeout(receiveMessage, 1000)
       }
-    );
-  }
-);
+  );
+  $(document).on("click", ".contacts__main .fa-search",
+      function() {
+        searchContact();
+      }
+  );
+  $(document).keyup(
+      function() {
+        if (event.which == 13) {
+          sendMessage();
+          setTimeout(receiveMessage, 1000)
+        }
+      }
+  );
+  $(document).keyup(
+      function() {
+        if (event.which == 13) {
+          searchMessage();
+        }
+  });
+});
+
+// FUNZIONI
+function sendMessage () {
+  var testoDaInviare = $('.message').val();
+  console.log(testoDaInviare);
+  var nuovoMessaggio = $('.template .messager').clone();
+  nuovoMessaggio.children('p').text(testoDaInviare)
+  console.log(nuovoMessaggio);
+  $('.chat__main').append(nuovoMessaggio)
+
+}
+
+function receiveMessage () {
+  var testoDaRicevere = 'ok';
+  console.log(testoDaRicevere);
+  var nuovoMessaggioRicevuto = $('.template .message_in').clone();
+  nuovoMessaggioRicevuto.children('p').text(testoDaRicevere)
+  $('.chat__main').append(nuovoMessaggioRicevuto)
+}
+function searchContact() {
+  var search = $(".search_input").val().toLowerCase();
+  var contact = $(".chat_friend");
+
+  contact.each(
+    function() {
+      var nomeContatto = $(this).find("h6").text().trim().toLowerCase();
+      if (!nomeContatto.includes(search)) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    }
+  );
+}
